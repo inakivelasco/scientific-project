4597edbb0c79039767c1f00975da0b50ea2efcdf
inakivelasco <77887322+IVR10@users.noreply.github.com>
2022-05-06 12:03:39 +0200

    train.py updated

--- train.py
+++ train.py
@@ -1,11 +1,11 @@
 import cv2, gc, json, os, random, torch
+
 from detectron2 import model_zoo
 from detectron2.config import get_cfg
-from detectron2.utils.visualizer import Visualizer
+from detectron2.data import build_detection_test_loader
 from detectron2.data.datasets import register_coco_instances
-from detectron2.engine import DefaultTrainer, DefaultPredictor
+from detectron2.engine import DefaultPredictor, DefaultTrainer
 from detectron2.evaluation import COCOEvaluator, inference_on_dataset
-from detectron2.data import MetadataCatalog, DatasetCatalog, build_detection_test_loader
 
 if __name__ == "__main__":
     gc.collect()
@@ -13,13 +13,13 @@ if __name__ == "__main__":
 
     TORCH_VERSION = ".".join(torch.__version__.split(".")[:2])
     CUDA_VERSION = torch.__version__.split("+")[-1]
-    print("torch: ", TORCH_VERSION, "; cuda: ", CUDA_VERSION)
-    print("cuda available: ", torch.cuda.is_available(), " ", torch.cuda.get_device_name(0))
+    print("torch:", TORCH_VERSION, "; cuda:", CUDA_VERSION)
+    print("cuda available:", torch.cuda.is_available(), " ", torch.cuda.get_device_name(0))
 
     # Model specification
     modelName = 'faster_rcnn_R_50_FPN_3x'
-    annotationName = 'Animal'
-    version = 2
+    annotationName = 'Animal'  # Only specific classes instead of all COCO dataset classes
+    version = 99
 
     dataDir = os.path.abspath(os.path.join(os.pardir, 'dataset'))
     annTrain = os.path.join(dataDir, 'annotations', f'instances{annotationName}_train2017.json')
@@ -28,30 +28,23 @@ if __name__ == "__main__":
     register_coco_instances("my_dataset_train", {}, annTrain, f'{dataDir}/images/train2017')
     register_coco_instances("my_dataset_val", {}, annVal, f'{dataDir}/images/val2017')
 
-    # Verify data loading
-    my_dataset_train_metadata = MetadataCatalog.get("my_dataset_train")
-    dataset_dicts = DatasetCatalog.get("my_dataset_train")
-    for i, d in enumerate(random.sample(dataset_dicts, 3)):
-        img = cv2.imread(d["file_name"])
-        visualizer = Visualizer(img[:, :, ::-1], metadata=my_dataset_train_metadata, scale=0.5)
-        vis = visualizer.draw_dataset_dict(d)
-    #     cv2.imshow(str(i), vis.get_image()[:, :, ::-1])
-    # cv2.waitKey()
-
+    # Model parameters & train
     cfg = get_cfg()
     cfg.merge_from_file(model_zoo.get_config_file(f"COCO-Detection/{modelName}.yaml"))
     cfg.DATASETS.TRAIN = ("my_dataset_train",)
     cfg.DATASETS.TEST = ()
+    cfg.TEST.EVAL_PERIOD = 0  # default 0
 
-    cfg.DATALOADER.NUM_WORKERS = 2
-    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(f"COCO-Detection/{modelName}.yaml")  # Let training initialize from model zoo
+    cfg.DATALOADER.NUM_WORKERS = 4
+    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(f"COCO-Detection/{modelName}.yaml")
     cfg.SOLVER.IMS_PER_BATCH = 2
-    cfg.SOLVER.BASE_LR = 0.00025  # (previous value: 0.00025) pick a good LR
+    cfg.SOLVER.BASE_LR = 0.00025
+    cfg.SOLVER.GAMMA = 0.1
 
-    # cfg.SOLVER.WARMUP_ITERS = 300
-    cfg.SOLVER.MAX_ITER = 1500    # 300 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
-    cfg.SOLVER.STEPS = ()       # do not decay learning rate, or when to decay it?
-    cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 64   # (previous value: 128) faster, and good enough for this toy dataset (default: 512)
+    cfg.SOLVER.WARMUP_ITERS = 0
+    cfg.SOLVER.MAX_ITER = 1500
+    cfg.SOLVER.STEPS = ()
+    cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 64
     cfg.MODEL.ROI_HEADS.NUM_CLASSES = 10
 
     cfg.OUTPUT_DIR = os.path.join(cfg.OUTPUT_DIR, f'{modelName}_{annotationName}_v{version}')
@@ -63,20 +56,10 @@ if __name__ == "__main__":
     trainer.resume_or_load(resume=False)
     trainer.train()
 
-    # Inference and evaluation
+    # Inference & evaluation
     cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
     cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.75
     predictor = DefaultPredictor(cfg)
-
-    dataset_dicts = DatasetCatalog.get("my_dataset_val")
-    my_dataset_metadata = MetadataCatalog.get("my_dataset_val")
-    for i, d in enumerate(random.sample(dataset_dicts, 3)):
-        im = cv2.imread(d["file_name"])
-        outputs = predictor(im)
-        v = Visualizer(im[:, :, ::-1], metadata=my_dataset_metadata, scale=0.8)
-        out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
-    #     cv2.imshow(f'image {i}', out.get_image()[:, :, ::-1])
-    # cv2.waitKey()
 
     evaluator = COCOEvaluator("my_dataset_val", cfg, False, output_dir=cfg.OUTPUT_DIR)
     val_loader = build_detection_test_loader(cfg, "my_dataset_val")

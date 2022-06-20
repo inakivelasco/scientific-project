7a3867566757b4183de3458f6b63f10fdd025c14
inakivelasco <77887322+inakivelasco@users.noreply.github.com>
2022-06-20 20:12:51 +0200

    Add fcos_R_50_FPN_1x_maize.py

---
+++ detectron2-code/configs/COCO-Detection/fcos_R_50_FPN_1x_maize.py
@@ -0,0 +1,20 @@
+from ..common.optim import SGD as optimizer
+from ..common.coco_schedule import lr_multiplier_1x as lr_multiplier
+# from ..common.data.coco import dataloader
+from ..common.data.maize import dataloader
+from ..common.models.fcos import model
+from ..common.train import train
+# from detectron2.config import LazyCall as L
+# solver = L()
+dataloader.train.mapper.use_instance_mask = False
+optimizer.lr = 0.001  # Default 0.01
+
+model.backbone.bottom_up.freeze_at = 2
+model.num_classes = 3  # Default 2
+dataloader.train.total_batch_size = 2  # Default 4
+
+# train.init_checkpoint = "/media/naeem/T7/trainers/fcos_R_50_FPN_1x.py/model_final.pth"
+# train.init_checkpoint = "detectron2://ImageNetPretrained/MSRA/R-50.pkl"
+train.init_checkpoint = "/Scientific-Project/models/fcos_R_50_FPN_1x_v00/model_final.pth"
+
+# print('hold')

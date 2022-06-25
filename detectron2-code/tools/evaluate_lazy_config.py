807389e88c4137871d690569c347d279a350dd04
inakivelasco <77887322+inakivelasco@users.noreply.github.com>
2022-06-25 20:42:16 +0200

    Update evaluate and train

--- detectron2-code/tools/evaluate_lazy_config.py
+++ detectron2-code/tools/evaluate_lazy_config.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import logging
 import os.path
+import sys
 
 from detectron2.checkpoint import DetectionCheckpointer
 from detectron2.config import LazyConfig, instantiate
@@ -20,10 +21,12 @@ from detectron2.utils.visualizer import Visualizer
 from detectron2.data import MetadataCatalog
 from detectron2.data.catalog import DatasetCatalog
 
+import GPUtil, psutil, threading, time, cpuinfo, json
 import cv2
 import numpy as np
 
 logger = logging.getLogger("detectron2")
+
 
 def register_dataset():
     from detectron2.data.datasets import register_coco_instances
@@ -48,6 +51,23 @@ def do_test(cfg, model):
     print_csv_format(ret)
     return ret
 
+def measure():
+    global stop_thread
+    t = threading.Timer(0.5, measure)
+    t.start()
+
+    if stop_thread:
+        t.cancel()
+
+    cpuPercent = psutil.cpu_percent()
+    ramPercent = psutil.virtual_memory().percent
+
+    GPUs = GPUtil.getGPUs()
+    gpusPercent = []
+    for i, gpu in enumerate(GPUs):
+        gpusPercent.append(gpu.memoryUtil * 100)
+    logging.getLogger('consumption').info(f'{cpuPercent} {ramPercent} {np.round(np.mean(gpusPercent), 1)}')
+
 
 args = default_argument_parser().parse_args()
 cfg = LazyConfig.load(args.config_file)
@@ -58,6 +78,20 @@ cfg.dataloader.test.num_workers = 0  # for debugging
 # cfg = LazyConfig.apply_overrides(cfg, args.opts)
 default_setup(cfg, args)
 register_dataset()
+
+logging.basicConfig(filename=os.path.join(os.pardir, os.pardir, 'models', model_name, 'evaluation', 'test',
+                                          'consumptionLog.txt'),
+                    filemode='w',
+                    format='',
+                    datefmt='',
+                    level=logging.INFO)
+loggerConsumption = logging.getLogger('consumption')
+logging.getLogger('consumption').info(f'CPU RAM GPU')
+stop_thread = False
+thread = threading.Thread(target=measure)
+thread.start()
+print('Starting process, 10 seconds to start...')
+time.sleep(10)
 
 model = instantiate(cfg.model)
 model.to(cfg.train.device)
@@ -71,6 +105,7 @@ os.makedirs(imagesDir, exist_ok=True)
 
 eval_loader = instantiate(cfg.dataloader.test)
 model.eval()
+
 class_labels = np.array(['Weeds', 'Maize', 'Bark'])  # HARDCODED
 for class_label in class_labels:
     os.makedirs(os.path.join(imagesDir, class_label), exist_ok=True)
@@ -90,8 +125,9 @@ for idx, inputs in enumerate(eval_loader):
     detected_scores = [round(score, 2) for score in detected_scores]
 
     labels = [f"{class_labels[detected_classes[i]]} {detected_scores[i]}" for i in range(len(detected_classes))]
-    out = v.overlay_instances(boxes=outputs[0]['instances'][outputs_mask]._fields['pred_boxes'].to("cpu").tensor.detach().numpy(),
-                              labels=labels)
+    out = v.overlay_instances(
+        boxes=outputs[0]['instances'][outputs_mask]._fields['pred_boxes'].to("cpu").tensor.detach().numpy(),
+        labels=labels)
 
     # cv2.imshow(f'image{idx}', out.get_image())
     cv2.imwrite(os.path.join(imagesDir, f'image{idx}.jpg'), out.get_image())
@@ -110,3 +146,27 @@ for idx, inputs in enumerate(eval_loader):
 
         cv2.imwrite(os.path.join(imagesDir, class_labels[classIdx], f'image{idx}.jpg'), out.get_image())
 
+# Hardware specs
+gpuNames = []
+for gpu in GPUtil.getGPUs():
+    gpuNames.append(gpu.name)
+gpuNames = '\n        '.join(gpuNames)
+hardwareSpecs = {
+        'cpu': {
+            'name': f'Model: {cpuinfo.get_cpu_info()["brand_raw"]}',
+            'logical_cores': f'Total nº of logical cores: {psutil.cpu_count()}',
+            'GHz': f'Speed: {np.round(psutil.cpu_freq().max/1000, 2)} GHz'
+        },
+        'ram': {
+            'GB': f'Total memory: {np.round(psutil.virtual_memory().total / 1024 ** 3, 2)} GB'
+        },
+        'gpu': {
+            'names': f'Models: {gpuNames}'
+        }
+    }
+with open(os.path.join(cfg.train.output_dir, 'hardwareSpecs.json'), 'w+') as f:
+    json.dump(hardwareSpecs, f)
+
+print('Finishing process, 10 seconds left...')
+time.sleep(10)
+stop_thread = True
